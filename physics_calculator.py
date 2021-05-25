65e9c160243aeb4edca55606e5e02eedc7ff6e4e
22persea15 <82135914+22persea15@users.noreply.github.com>
2021-05-25 09:50:48 +0900

    Commit 1

---
+++ physics_calculator.py
@@ -0,0 +1,43 @@
+
+#Imports
+import os
+import tkinter as tk
+import math
+
+#Create Root ------------
+#root = tk.Tk()
+#root.title("Physics Calculator")
+#root.geometry("475x350")
+
+#Classes ------------
+
+#----- AP Physics 1
+#kinematics
+#dynamics
+#circular motion
+#energy
+#momentum
+#simple harmonic motion
+#torque / rotational motion
+#electricity
+#mechanical waves and sound
+
+#----- AP Physics 2
+#fluids
+#thermodynamics
+#electric force, field and potential 
+#electric circuits
+#magnetism and electromagnetic induction
+#geometric and physical optics
+#quantam, atomic, and nuclear physics
+
+
+#Functions ------------
+
+#Widgets ------------
+
+#Mainloop ------------
+
+
+#root.mainloop()
+
